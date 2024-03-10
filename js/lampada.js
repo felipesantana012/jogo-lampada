ee8a2004589c4d93d7c8eef06e7d72ec7330d857
Felipe Santana <felipe012santana012@gmail.com>
2024-03-10 20:47:18 -0300

    correção de bug do sorteador

--- js/lampada.js
+++ js/lampada.js
@@ -9,12 +9,16 @@ const sortearInterruptor = () =>  {
     return interruptor;
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    window.interruptor = sortearInterruptor();
+});
+
+
 
 const acendeApaga = () => {
     const lampada = document.getElementById("lampada");
     const lampadaDesligada = "img/lampadaDesligada.jpg";
     const lampadaLigada = "img/lampadaLigada.jpg";
-    const interruptor = sortearInterruptor();
  
     if(interruptor.checked){
         lampada.src = lampadaLigada;
